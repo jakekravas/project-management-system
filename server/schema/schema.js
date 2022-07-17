9107844997ffd119539c9d2588a11fdf5d85d498
Jake Kravas <jakekravas@Jakes-MacBook-Air.local>
2022-07-17 00:09:11 -0700

    added functionality for deleting a client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -93,8 +93,18 @@ const mutation = new GraphQLObjectType({
 
         return client.save();
       }
+    },
+    // Delete a client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndDelete(args.id);
+      }
     }
-  }
+  },
 });
 
 module.exports = new GraphQLSchema({
