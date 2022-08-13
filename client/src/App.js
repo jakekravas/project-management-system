682146150c5a7a489820b235ac5c9650539debb3
Jake Kravas <jakekravas@Jakes-MacBook-Air.local>
2022-08-12 20:44:01 -0700

    can now add clients on front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import Header from './components/Header';
 import Clients from './components/Clients';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import AddClientModal from './components/AddClientModal';
+
+// prevents warning from displaying in console when using cache to get clients
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming ) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming ) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
-})
+  cache,
+});
 
 function App() {
   return (
@@ -13,6 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header/>
         <div className='container'>
+          <AddClientModal/>
           <Clients/>
         </div>
       </ApolloProvider>
